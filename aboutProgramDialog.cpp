a3973636d8ee6f915963fdc80f9c0b16e9042c91
Valery <cweic002@mail.ru>
2023-07-20 20:56:17 +0300

    Добавлены изменения в меню справка о программе
    удалены в диалоге размеры, добавлено масштабирование QPixmap по высоте добавлены 3 Qlabela
    1-windows логотип
    1-labelWindows10Image логотип
    2-QLabelFrame рамка между показывающая отступ небольшой
    3- labelAbout информация о программе
    
    vBoxLayout - для компоновки 3 Qlabel

--- aboutProgramDialog.cpp
+++ aboutProgramDialog.cpp
@@ -3,21 +3,60 @@
 AboutProgramDialog::AboutProgramDialog(QWidget * parent):QDialog(parent){
     setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint |
                    Qt::WindowCloseButtonHint | Qt::MSWindowsFixedSizeDialogHint );//Qt::MSWindowsFixedSizeDialogHint
-    this->setMaximumSize(572, 484);
-    this->resize(572, 484);
     this->setWindowTitle("Блокнот: сведения");
 
     QPixmap pix;
     pix.load(":/images/windows10.png");
 
+    pix = pix.scaledToHeight(75,Qt::SmoothTransformation);
 
-    QString textHtml;
-    textHtml="<center><img width=\"300\" align=\"\" src=\":images/windows10.png\"></center>";
+    QLabel * labelWindows10Image = new QLabel();
+    labelWindows10Image->setPixmap(pix);
+    labelWindows10Image->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
-    QLabel * labelAbout = new QLabel(this);
-    labelAbout->resize(572,100);
-    labelAbout->setText(textHtml);
+    QLabel * QLabelFrame =new QLabel();
+    QLabelFrame->setFrameStyle(QFrame::Box | QFrame::Raised);
+    QLabelFrame->setMaximumHeight(1);
 
+    QLabel * labelAbout = new QLabel();
+
+    labelAbout->setText("<html>"
+                        "<head>"
+                        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">"
+                        "<title>О программе</title>"
+                        "</head>"
+                        " <style>"
+                        "p{"
+                            "margin-left:10px;"
+                        "}"
+                        "body{"
+                            "background-color: red;"
+                        "}"
+                        "</style>"
+                        "<body>"
+                        "<h4>Cweic002</h4>"
+                        "<p>Программное обеспечение было написанно на фреймворке Qt.В качестве примера была"
+                        "<br>использованна программа notepad включенная в состав операционной системы Microsoft Windows."
+                        "<br>Данная пограмма была создана с целью демонстраций своих возможностей и не является"
+                        "<br>коммерческим продуктом.</p>"
+                        "<p align=\"right\">С уважением, Cweic002</p>"
+                        "</body>"
+                        "</html>");
+    labelAbout->setStyleSheet("");
+    QVBoxLayout * vBoxLayoutAbout = new QVBoxLayout();
+    vBoxLayoutAbout->addWidget(labelAbout);
+
+    QPushButton * PushButtonOk = new QPushButton("Ок");
+    PushButtonOk->setSizePolicy(QSizePolicy::Maximum,QSizePolicy::Minimum);
+    connect(PushButtonOk,&QPushButton::clicked,this,&AboutProgramDialog::accept);
+
+    QVBoxLayout * vBoxLayout = new QVBoxLayout();
+    vBoxLayout->addWidget(labelWindows10Image);
+    vBoxLayout->addWidget(QLabelFrame);
+    vBoxLayout->addLayout(vBoxLayoutAbout);
+    vBoxLayout->addWidget(PushButtonOk,1,Qt::AlignmentFlag::AlignRight);
+
+    this->setLayout(vBoxLayout);
 }
 
 void AboutProgramDialog::closeEvent(QCloseEvent *){
